60b3eec1cc36688a61bcae2fe930026d73a72f8c
Mirza Anzar Baig <mabaig222@gmail.com>
2025-01-15 17:55:42 +0530

    competency work 13

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className="w-full bg-black h-16 ">
-      <div className="flex items-center justify-start md:justify-center lg:justify-start h-full md:ml-32">
+      <div className="flex items-center justify-start md:justify-center lg:justify-start h-full p-4 px-8">
         <Link href="/" className="flex items-center">
           <Image src={"/ABG.png"} width={84} height={69} />
         </Link>
